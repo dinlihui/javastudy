6ee710895ae5894c96d828e89fed2c46096acb63
dinglihui <156963866@qq.com>
2020-09-27 14:09:45 +0800

    study

---
+++ src/study/xunhuan.java
@@ -0,0 +1,12 @@
+package study;
+
+public class xunhuan {                        //類聲明
+    public static void main (String args[]){
+        int sum = 0,i = 0,j = 0;
+        for(i = 1;i <= 100;i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
