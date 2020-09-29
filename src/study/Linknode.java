4394791862d397b8bdf7c1cdc640e5fe4fe60271
dinglihui <156963866@qq.com>
2020-09-29 17:30:46 +0800

    2020 9 29

---
+++ src/study/Linknode.java
@@ -0,0 +1,24 @@
+package study;
+
+public class Linknode {
+
+    class LinkNode<E> {
+        E data;
+        LinkNode<E> next;
+
+        public LinkNode(E d) {
+            this.data = d;
+            this.next = null;
+        }
+
+        public LinkNode() {
+            this.next = null;
+        }
+
+        public LinkNode(E data, LinkNode<E> next) {
+            this.next = next;
+            this.data = data;
+        }
+    }
+
+}
