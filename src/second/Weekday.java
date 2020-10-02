a53a6e315bf526b6f0a3367e86e6e6a6295adb2b
dinglihui <156963866@qq.com>
2020-10-02 21:54:31 +0800

    2020 10 2

--- src/second/Weekday.java
+++
@@ -1,5 +0,0 @@
-package second;
-
-public enum Weekday {
-    星期一，星期二，星期三，星期四，星期五，星期六，星期日
-}
---
+++ src/third/Example_2.java
@@ -0,0 +1,21 @@
+package third;
+
+public class Example_2 {
+    public static void main(String args[]){
+        int math = 65, english = 85;
+        if(math>60){
+            System.out.println("数学及格了");
+        }
+        else{
+            System.out.println("数学不及格了");
+        }
+        if(english>90){
+            System.out.println("英语是优秀");
+
+        }
+        else{
+            System.out.println("英语不是优秀");
+        }
+        System.out.println("我在学习if-else语句");
+    }
+}
