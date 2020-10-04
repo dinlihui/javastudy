7c22d8ed1b8b0fecffa63a0e91f4d8d824fd6454
dinglihui <156963866@qq.com>
2020-10-04 16:50:44 +0800

    2020 10 4

---
+++ src/fourth/Example_12.java
@@ -0,0 +1,11 @@
+package fourth;
+
+import java.util.Date;
+
+public class Example_12 {
+    public static void main(String args[]){
+        Date date = new Date();
+        System.out.println("本地机器时间：");
+        System.out.println(date);
+    }
+}
